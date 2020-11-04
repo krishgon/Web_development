70b267d2ee6ef798d0d6919ec3b95a856f10cd43
krishgon <krishgondia@gmail.com>
2020-11-04 12:55:12 +0530

    Update homepage

---
+++ cs50/homepage/script.js
@@ -0,0 +1,11 @@
+function changeColor(){
+    body = document.body
+
+    body.style.backgroundColor = "red"
+}
+
+function focusNavbar(){
+    content = document.getElementById("body")
+
+    content.style.opacity = "0.2";
+}
